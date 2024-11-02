ca8bb59a526e4fb43d7bb5e63c6b92343bca41c1
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2024-11-03 00:12:55 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+
+import (
+   "encoding/csv"
+   "fmt"
+   "log"
+   "os"
+
+   "github.com/gocolly/colly"
+)
+
+
+type Book struct {
+    Title string
+    Price string
+}
+
+
+func BooksScraper() {
+    fmt.Println("Start scraping")
+    file, err := os.Create("export.csv")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+    writer := csv.NewWriter(file)
+    defer writer.Flush()
+    headers := []string{"Title", "Price"}
+    writer.Write(headers)
+    c := colly.NewCollector(
+        colly.AllowedDomains("books.toscrape.com"),
+    )
+
+    // proxyUsername := "<Username>"
+    // proxyPassword := "<Password>"
+    // proxyUrl := fmt.Sprintf("http://customer-%s:%s@pr.oxylabs.io:7777", proxyUsername, proxyPassword)
+    // c.SetProxy(proxyUrl)
+
+    c.OnHTML(".product_pod", func(e *colly.HTMLElement) {
+        book := Book{}
+        book.Title = e.ChildAttr(".image_container img", "alt")
+        book.Price = e.ChildText(".price_color")
+        row := []string{book.Title, book.Price}
+        writer.Write(row)
+    })
+
+    c.OnHTML(".next a", func(e *colly.HTMLElement) {
+        nextPage := e.Attr("href")
+        c.Visit(e.Request.AbsoluteURL(nextPage))
+    })
+
+    c.OnResponse(func(r *colly.Response) {
+        fmt.Println(r.StatusCode)
+    })
+    c.OnRequest(func(r *colly.Request) {
+        fmt.Println("Visiting", r.URL)
+    })
+    c.Visit("https://books.toscrape.com/")
+}
+
+func main() {
+    BooksScraper()
+}
